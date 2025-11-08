d0ac7c769173f871891704976ef875a7eb968d0e
XenaCodeOfficial <contacto@xenacode.com>
2025-11-08 02:23:11 +0100

    Change profile menu text to French - Mon Profil

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -159,7 +159,7 @@ export default function Header({ user, role }: HeaderProps) {
                       <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                       </svg>
-                      Profil
+                      Mon Profil
                     </Link>
                     <button
                       onClick={handleLogout}
